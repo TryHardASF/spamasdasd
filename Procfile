d72f9d38bd5211a035dc33cdbd825647fd516fce
TryHardASF <62212428+TryHardASF@users.noreply.github.com>
2020-05-23 04:24:17 +0200

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,20 @@
+worker: node index.js
+worker1: node bot1.js
+worker2: node bot2.js
+worker3: node bot3.js
+worker4: node bot4.js
+worker5: node bot5.js
+worker6: node bot6.js
+worker7: node bot7.js
+worker8: node bot8.js
+worker9: node bot9.js
+worker10: node bot10.js
+worker11: node bot11.js
+worker12: node bot17.js
+worker13: node bot12.js
+worker14: node bot13.js
+worker15: node bot14.js
+worker16: node bot15.js
+worker17: node bot16.js
+
+web: npm start
---
+++ bot1.js
@@ -0,0 +1,87 @@
+var ServerID = "709075855296692244"; //اي دي السيرفر
+var ChannelID = "709075855787425854";// اي دي الروم
+
+
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+client.on("message", async message => {
+    const prefix = "!!";
+
+    // If the author's a bot, return
+    // If the message was not sent in a server, return
+    // If the message doesn't start with the prefix, return
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // Arguments and command variable
+    // cmd is the first word in the message, aka the command
+    // args is an array of words after the command
+    // !say hello I am a bot
+    // cmd == say (because the prefix is sliced off)
+    // args == ["hello", "I", "am", "a", "bot"]
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === "ping") {
+        // Send a message
+        const msg = await message.channel.send(`🏓 Pinging....`);
+
+        // Edit the message
+        msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
+    }
+
+    if (cmd === "say") {
+        // Check if you can delete the message
+        if (message.deletable) message.delete();
+
+        if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
+        
+        // Role color
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
+                .setTimestamp()
+                .setImage(client.user.displayAvatarURL)
+                .setAuthor(message.author.username, message.author.displayAvatarURL);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+    }
+});
+
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
+client.login(process.env.BOT_TOKEN1);
